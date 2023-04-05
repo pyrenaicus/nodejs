368244a7d7338c852b4cd26236444ffc6989d603
Section1 <otinoco@digiocio.com>
2023-04-05 12:12:58 +0200

    CODE 7 - Exemples CRUD

---
+++ app.js
@@ -0,0 +1,32 @@
+"use strict"
+
+const express = require('express');
+var bodyParser = require("body-parser");
+
+const app = express();
+
+//Carregar rutes
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//Configurar capçaleres
+
+//Rutes base
+app.get("/get",(req,res) => {
+    res.send("Soc un get");
+})
+
+app.post("/post",(req,res) => {
+    res.send("Soc un post");
+})
+
+app.put("/put",(req,res) => {
+    res.send("Soc un put");
+})
+
+app.delete("/delete",(req,res) => {
+    res.send("Soc un delete");
+})
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 "use strict" //Per obtenir compatibilitat amb les ultimes versions
 
-const express = require('express');
 var mongoose = require("mongoose");
+const app = require("./app");
+const port = 3000;
 
 try {
   mongoose.connect('mongodb://localhost:27017/curs_nodejs');
   console.log("La base de dades está funcionant correctamente");
+  app.listen(port, () => {
+    console.log("Exemple d'app escoltant");
+  });
 } catch (error) {
   console.log("La base de dades no está funcionant correctamente" + error);
 }
-
-const app = express();
-const port = 3000;
-
-app.get("/",(req,res) => {
-    res.send("Hola mòn");
-})
-
-app.listen(port, () => {
-  console.log("Exemple d'app escoltant");
-});
