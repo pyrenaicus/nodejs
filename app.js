539df48356ebee7d9527c1fdb073995ba5084bc1
Section1 <otinoco@digiocio.com>
2023-04-05 12:48:02 +0200

    CODE 13 - Rutes i subrutes

--- app.js
+++ app.js
@@ -6,52 +6,15 @@ var bodyParser = require("body-parser");
 const app = express();
 
 //Carregar rutes
+var user_routes = require("./routes/usuari");
 
 //Middlewares o tasques que s'han de realitzar abans de rebre la petició
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-const testFunction = () => {
-    //console.log("Log from the testFunction")
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(true)
-        },3000)
-    })
-}
-
-app.use( async (req, res, next) => {
-    //setTimeout(() => {
-        //console.log("middleware 1");
-        const resProm = await testFunction();
-        console.log("promise res", resProm);
-        next();
-    //},2000)
-})
-
-app.use((req, res, next) => {
-    console.log("middleware 2");
-    next();
-})
-
 //Configurar capçaleres
 
 //Rutes base
-app.get("/get",(req,res) => {
-    res.send("Soc un get");
-})
-
-app.post("/post",(req,res) => {
-    //res.send("Soc un post")
-    res.json(req.body) //Retornem en forma de Json el que ens hagin enviat
-})
-
-app.put("/put",(req,res) => {
-    res.send("Soc un put");
-})
-
-app.delete("/delete",(req,res) => {
-    res.send("Soc un delete");
-})
+app.use("/api", user_routes); //Definim una ruta base sobre la que treballar
 
 module.exports = app;
