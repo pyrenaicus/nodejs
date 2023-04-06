59a223329bd8a4495a62a787b8d575d4ff3e8fed
Section1 <otinoco@digiocio.com>
2023-04-06 14:54:20 +0200

    CODE 34 - Creant un Middleware personalitzat

---
+++ middleware/authenticated.js
@@ -0,0 +1,29 @@
+//Realitzem un middleware personalitzat per validar previ a executar el controlador corresponent la validació del token 
+var jwt = require("jwt-simple"); 
+var moment = require("moment"); //Executem npm i moment
+var secret = "cibernarium";
+
+exports.ensureAuth = function(req, res, jwt){
+    //recollir l'autorització amb un header o encapçalament
+    if(!req.headers.authorization){
+        return res.status(403).send({message: "L'autorització no te la capçalera d'atentificació"})
+    }
+
+    var token = req.headers.authorization.replace(/['"]+/g, ''); //Recupedrem el token i treiem caracters especials
+
+    try{
+        var payload = jwt.decode(token, secret); //Aportem el token i el secret per decodificar-lo
+        //Validem si ha caducat el token
+        if(payload.exp <= moment().unix()){
+            return res.status(401).send({message: "El token ha expirat"});
+        }
+    }catch(ex){
+        console.log(ex);
+        returnres.status(404).send({message: "El token no és vàlid"});
+    }
+
+    //Afegim dins de la petició totes les dades del usuari
+    req.user = payload;
+
+    next();
+}
