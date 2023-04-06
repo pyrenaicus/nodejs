8f6f0c5e660023424ae7ea5660158f301c83797f
Section1 <otinoco@digiocio.com>
2023-04-06 14:36:39 +0200

    CODE 32 - Creem el servei JWT

---
+++ services/jwt.js
@@ -0,0 +1,19 @@
+//Creem un arxiu per realitzar els mètodes que treballaran amb JWT
+var jwt = require("jwt-simple"); //No oblidem de instal.lar la llibreria bcrypt, npm i bcrypt
+//Anem a realitzar el Payload que és l'objecte a on contindrem el token i a on situarem la data de creació del token i la de caducitat
+var moment = require("moment");
+var secret = "cibernarium";
+
+exports.createToken = function(usuari){
+    var payload = {
+        //creem un objecte json
+        sub: usuari._id,
+        name: usuari.nom,
+        surname: usuari.cognom,
+        email: usuari.email,
+        iat: moment().unix(), //Format Timestamp
+        exp: moment().add(30, 'days').unix()
+    };
+
+    return jwt.encode(payload, secret);
+};
